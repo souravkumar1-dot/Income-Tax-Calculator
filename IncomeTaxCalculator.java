5d32ab5516ced9749df0ecf2ef49c828ceb0b26b
Sourav Kumar <souravkumarsingh077@gmail.com>
2026-07-20 09:53:40 +0530

    Add files via upload

---
+++ IncomeTaxCalculator.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class IncomeTaxCalculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int income = sc.nextInt();
+        int tax;
+
+        if (income <= 10000) {
+            tax = (income * 5) / 100;
+        } else if (income <= 50000) {
+            tax = (income * 10) / 100;
+        } else {
+            tax = (income * 20) / 100;
+        }
+
+        System.out.println(tax);
+    }
+}
